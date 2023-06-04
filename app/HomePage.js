edf9488471fa75c907576b60802244cb43873153
JRH89 <thegemlins@gmail.com>
2023-06-04 08:48:57 -0700

    UI UX

--- app/HomePage.js
+++ app/HomePage.js
@@ -1,69 +1,71 @@
-import React, { useState } from 'react'
-import Image from 'next/image'
-import QRious from 'qrious'
+import React, { useState } from 'react';
+import QRCode from 'react-qr-code';
 
 function Home() {
-  const [inputValue, setInputValue] = useState('')
-  const [inputType, setInputType] = useState('url')
-  const [qrCodeUrl, setQrCodeUrl] = useState('')
- 
+  const [inputValue, setInputValue] = useState('');
+  const [inputType, setInputType] = useState('url');
+  const [qrCodeUrl, setQrCodeUrl] = useState('');
+  const [foreground, setForeground] = useState('#000000');
+  const [background, setBackground] = useState('#FFFFFF');
+  const [qrValue, setQrValue] = useState('');
+
   const handleSubmit = (event) => {
-    event.preventDefault()
-    let qrValue
+    event.preventDefault();
+
+    let qrValue;
 
     if (inputType === 'url' || inputType === 'text') {
-      qrValue = inputValue
+      qrValue = inputValue;
     }
-      
-    // Generate QR code based on input value and type
-    const qr = new QRious({
-      value: qrValue,
-      size: 256,
-      level: 'L',
-      padding: null,
-    })
-    setQrCodeUrl(qr.toDataURL());
 
-    // Clear input value and image file
-    setInputValue('')
-   
-  }
+    // Generate QR code based on input value and type
+    setQrValue(qrValue);
+    setQrCodeUrl(qrValue);
+    setInputValue('');
+  };
 
   return (
-    <>
-      <div className='my-auto relative place-items-center items-center flex flex-col justify-center align-middle'>
-        <form onSubmit={handleSubmit} className='flex flex-col text-center justify-center'>
-          <label className='underline justify-center align-middle items-centertext-xl mb-2'>
-              CHOOSE:
-          </label>
-            <div className="justify-evenly pl-2 mb-2 flex flex-row align-middle place-items-center">
-            <label className="flex justify-center text-center items-center align-middle mr-4">
-            <input className='justify-center text-center items-center' type="checkbox" name="inputType" value="url" checked={inputType === 'url'} onChange={() => setInputType('url')} />
-              URL
-            </label>
+    <div className='flex select-none flex-col bg- h-screen justify-center items-center'>
+      <form onSubmit={handleSubmit} className='flex flex-col text-center items-center justify-center'>
 
-            <h1>or...</h1>
-            <label className="justify-center flex align-middle text-center items-center mr-4">
-              <input type="checkbox" name="inputType" value="text" checked={inputType === 'text'} onChange={() => setInputType('text')} />
-              Text
-            </label> 
-            </div>
-          <input autoComplete='url' className='rounded-md max-w-[60ch] flex flex-col shadow-md shadow-white/60 justify-center align-center items-center text-white bg-black border-2 border-solid border-white text-center' type='any' value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
-          <button className='shadow-md font-bold shadow-white/60 mb-4 border-2 border-solid border-white rounded-md mt-4' type='submit'>CREATE</button>
-        </form>
-        
-          {qrCodeUrl && (
-            <div className='justify-between align-middle items-center flex'>
-              <div className='align-middle ml-8 mr-8 rounded-md bg-white flex justify-center' id='qrContainer'>
-              <Image className='w-90 p-4 flex justify-center items-center' src={qrCodeUrl} alt="QR Code" width={256} height={256} />
-              </div>
-            </div>
-          )}
-      </div>
-    </>
-  )
+        <div className="flex justify-evenly mb-2">
+          <label className="flex items-center mr-4">
+            <input className='mr-2' type="checkbox" name="inputType" value="url" checked={inputType === 'url'} onChange={() => setInputType('url')} />
+            URL
+          </label>
+          <h1>or</h1>
+          <label className="flex items-center ml-4">
+            <input className='mr-2' type="checkbox" name="inputType" value="text" checked={inputType === 'text'} onChange={() => setInputType('text')} />
+            Text
+          </label>
+        </div>
+        <div className="flex justify-center">
+          <input autoComplete='url' className='rounded-md w-60 py-2 px-4 mb-4 text-black text-center' type='text' value={inputValue} onChange={(event) => setInputValue(event.target.value)} placeholder='Enter URL or text' />
+        </div>
+        <div className='flex flex-col gap-4 mb-4'>
+          <label className='mr-2'>
+            Foreground Color:
+            <input type='color' value={foreground} onChange={(event) => setForeground(event.target.value)} className='ml-2' />
+          </label>
+          <label>
+            Background Color:
+            <input type='color' value={background} onChange={(event) => setBackground(event.target.value)} className='ml-2' />
+          </label>
+        </div>
+        <button className='py-2 px-4 border border-white shadow-lg shadow-black hover:scale-90 hover:opacity-50 duration-300 w-60 text-center place-items-center font-bold bg-black text-white rounded-md mt-2' type='submit'>CREATE</button>
+      </form>
+
+      {qrCodeUrl && (
+        <div className='mt-8'>
+          <div className='bg-white p-4 rounded-md shadow-md' id='qrContainer'>
+            <QRCode value={qrValue} size={256} fgColor={foreground} bgColor={background} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default function ClientHome() {
-    return <Home />
+  return <Home />;
 }
--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ import Footer from './Footer'
 
 export default function Page() {
   return (
-    <div className='flex flex-col min-h-screen place-items-center justify-center'>
-    <Header />
-    <main  className='flex flex-col justify-center place-items-center items-center align-middle' > 
-      <ClientHome /> 
-    </main>
-    <Footer />
+    <div className='flex flex-col place-content-center items-center place-items-center justify-center'>
+      <Header />
+      <main className='flex flex-col justify-center place-items-center items-center place-content-center align-middle' >
+        <ClientHome />
+      </main>
+      <Footer />
     </div>
   )
 }
