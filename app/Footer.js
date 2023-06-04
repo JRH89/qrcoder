47e35c4390389579a7ef735933842f5e47c3915a
JRH89 <thegemlins@gmail.com>
2023-06-04 08:51:51 -0700

    UI

--- app/Footer.js
+++ app/Footer.js
@@ -2,12 +2,12 @@ import Link from 'next/link'
 import React from 'react'
 
 function Footer() {
-    return (
-      <div className='text-center w-full justify-center flex'>
-        <div className='mt-4 border-t w-full border-solid border-white absolute mx-auto my-auto items-center align-center justify-center text-center bottom-0'>
-          <Link className='hover:scale-50 hover:opacity-60' href="https://jaredsportfolio.netlify.app">Hooker Hill Studios © 2023</Link>
-        </div>
+  return (
+    <div className='text-center absolute bottom-0 w-full justify-center flex'>
+      <div className='mt-4 border-t w-full border-solid border-white mx-auto my-auto items-center align-center justify-center text-center'>
+        <Link className='hover:scale-50 hover:opacity-60' href="https://jaredsportfolio.netlify.app">Hooker Hill Studios © 2023</Link>
       </div>
+    </div>
   )
 }
 
