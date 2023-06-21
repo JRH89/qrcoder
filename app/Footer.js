4a395c289ce1aa2e82b26e6b52cee417e9fe0ace
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-06-20 17:56:06 -0700

    Update Footer.js

--- app/Footer.js
+++ app/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='text-center fixed bottom-0 w-full justify-center flex'>
       <div className='border-t p-2 border-solid border-white  items-center align-center justify-center text-center'>
-        <Link className='hover:scale-50 hover:opacity-60' href="https://jaredsportfolio.netlify.app">Hooker Hill Studios © 2023</Link>
+        <Link className='hover:scale-50 hover:opacity-60' href="https://hooker-hill-studios.vercel.app.app">Hooker Hill Studios © 2023</Link>
       </div>
     </div>
   )
