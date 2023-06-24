9dd36b6c18d643bc034828850507f3a91e3fa221
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-06-23 21:16:44 -0700

    Update Header.js

--- app/Header.js
+++ app/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='fixed top-0 w-full font-extrabold  flex flex-col justify-center text-center'>
-      <h1 className='text-3xl p-2 border-b border-solid border-whit top-0 flex flex-col text-center mx-auto my-auto'>
+      <h1 className='text-3xl p-2 top-0 flex flex-col text-center mx-auto my-auto'>
         QR Coder
       </h1>
     </div>
