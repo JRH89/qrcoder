4a465b93cd4cda7769cf93a302415d8fd4fe7f0e
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-10-12 22:18:38 -0700

    Update Footer.js

--- app/Footer.js
+++ app/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='text-center fixed bottom-0 w-full justify-center flex'>
       <div className='p-2 items-center align-center justify-center text-center'>
-        <Link className='hover:scale-50 hover:opacity-60' href="https://hooker-hill-studios.vercel.app">Hooker Hill Studios</Link>
+        <Link className='hover:scale-50 hover:opacity-60' href="https://www.hookerhillstudios.com">Hooker Hill Studios</Link>
       </div>
     </div>
   )
